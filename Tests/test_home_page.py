a9fb60ef253132c4ba244c1fb560531aef9055cc
Kamil Roslan <kamil.roslan@globallogic.com>
2022-11-03 09:02:01 +0100

    Add materials page tests

--- Tests/test_home_page.py
+++ Tests/test_home_page.py
@@ -10,13 +10,23 @@ class HomePageTests:
     @mark.parametrize("section", sections)
     def test_sessions(self, chrome_driver, login, section):
         home_page = HomePage(driver=chrome_driver)
-        home_page.sessions_button.click()
         action = ActionChains(chrome_driver)
+        home_page.sessions_button.click()
         action.move_to_element(
-            home_page.find_session(TE_subsection).web_element
+            home_page.find_element_in_dropdown(TE_subsection).web_element
         ).perform()
 
         action.move_to_element(
-            home_page.find_session(section).web_element
+            home_page.find_element_in_dropdown(section).web_element
         ).click().perform()
         assert home_page.page_title.text() == section
+
+    @mark.parametrize("section, title", materials_param)
+    def test_materials(self, chrome_driver, login, section, title):
+        action = ActionChains(chrome_driver)
+        home_page = HomePage(driver=chrome_driver)
+        home_page.materials_button.click()
+        action.move_to_element(
+            home_page.find_element_in_dropdown(section).web_element
+        ).click().perform()
+        assert home_page.page_title.text() == title
